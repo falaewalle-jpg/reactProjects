18ad01eeddf0f5452a7612d3745a251065eb7c3b
Falae Walle <falaewalle@gmail.com>
2025-09-29 12:54:22 +0530

    adding currencyconverter

---
+++ currencyconverter/src/hooks/useV.js
@@ -0,0 +1,17 @@
+import { useEffect,useState } from "react";
+import React from 'react'
+
+function useV(currency) {
+    const [data,setData] = useState({});
+    useEffect(()=>{
+        fetch(`https://api.exchangerate-api.com/v4/latest/${currency}`)
+        .then((res)=>res.json())
+        .then((res)=>setData(res.rates))
+        console.log(data);
+
+    },[currency]);
+    console.log(data);
+    return data;
+}
+
+export default useV;
